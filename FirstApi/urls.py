a0421c6f2b13f576a561b7586d591e2221c64dcd
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2023-05-15 13:00:31 -0700

    Fourth day

--- FirstApi/urls.py
+++ FirstApi/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/', include('products.urls'))
+    path('product/', include('products.urls')),
+    path('newapp/', include('newapp.urls'))
 ]
