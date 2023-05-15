83498ff3eac47157efd6ba4ebe443ff2df85560a
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2023-05-15 13:43:26 -0700

    create the models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    category = models.CharField(max_length=100)
+    decription = models.CharField(max_length=1000)
+    manufacture = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    timeStamp = models.DateTimeField(
+        auto_now_add=True, auto_now=False, blank=True)
+
+    def __str__(self):
+        return self.name
